10649cc08f5cc7f59698aaf8a8e884e180c04b63
plocki <49858229+officialPlocki@users.noreply.github.com>
2021-11-07 19:09:05 +0100

    update

--- LibaryPaper/src/main/java/me/refluxo/libary/spigot/utils/cloud/GameInstance.java
+++ LibaryPaper/src/main/java/me/refluxo/libary/spigot/utils/cloud/GameInstance.java
@@ -1,4 +1,19 @@
 package me.refluxo.libary.spigot.utils.cloud;
 
+import de.dytanic.cloudnet.driver.CloudNetDriver;
+import de.dytanic.cloudnet.driver.service.ServiceConfiguration;
+import de.dytanic.cloudnet.driver.service.ServiceInfoSnapshot;
+import de.dytanic.cloudnet.driver.service.ServiceTemplate;
+
 public class GameInstance {
+
+    public void startInstance() {
+        ServiceInfoSnapshot snapshot = CloudNetDriver.getInstance().getCloudServiceFactory().createCloudService(ServiceConfiguration.builder()
+                .serviceId(null)
+                .task("task")
+                .staticService(true)
+                .taskId(1)
+                .addTemplates(ServiceTemplate.parse("")).build());
+    }
+
 }
